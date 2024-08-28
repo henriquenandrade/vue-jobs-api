039cadeb0aeed80730d25a5cc97b455a39b4171e
Henrique Nascimento <henriquenascandrade@outlook.com>
2024-08-28 16:15:18 -0300

    first commit

---
+++ app/Models/Company.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+    ];
+
+    public function vacancies()
+    {
+        return $this->hasMany(Vacancy::class);
+    }
+}
