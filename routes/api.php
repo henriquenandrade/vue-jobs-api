754c79ef485a55e9b9196ab5949dcd937c5c8910
Henrique Nascimento <henriquenascandrade@outlook.com>
2024-08-29 07:30:51 -0300

    add register route group and add vacancy middleware route group

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,19 @@
 <?php
 
-use App\Http\Controllers\VacancyController;
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\VacancyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-Route::resource('jobs', VacancyController::class);
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::resource('jobs', VacancyController::class);
+});
