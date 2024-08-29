b004b3dd0ce2f6285a1ee904f772e2341ac71ba9
Henrique Nascimento <henriquenascandrade@outlook.com>
2024-08-29 07:29:01 -0300

    add resources

---
+++ app/Http/Resources/VacancyResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class VacancyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'type' => $this->type,
+            'description' => $this->description,
+            'location' => $this->location,
+            'salary' => $this->salary,
+            'company' => $this->company_id,
+            'created_at' => $this->created_at->format('d/m/Y'),
+            'updated_at' => $this->updated_at->format('d/m/Y'),
+        ];
+    }
+}
