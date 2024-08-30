271ec493fd0ea614ce3bc9dfca5c8a04f099c615
Henrique Nascimento <henriquenascandrade@outlook.com>
2024-08-30 09:21:06 -0300

    add company resource

---
+++ app/Http/Resources/CompanyResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CompanyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $request->id,
+            'name' => $request->name,
+            'description' => $request->description,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'created_at' => $this->created_at->format('d/m/Y'),
+            'updated_at' => $this->updated_at->format('d/m/Y'),
+        ];
+    }
+}
